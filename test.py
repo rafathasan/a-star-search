c1e419a4fd0ed423a93721214aeefee8d3e6ae3c
rafathasan <rafathasankhan@gmail.com>
2020-09-12 02:25:32 +0600

    test.py edited

--- test.py
+++ test.py
@@ -7,10 +7,9 @@ with open("dataset.csv") as f:
     headers = next(reader)
     nth = 0
     for row,header in zip(reader,headers):
-        data[header] = row
-        heuristic[header] = row[nth]
+        data[header] = [int(x) for x in row]
+        heuristic[header] = int(row[nth])
         nth+=1
 
-print(data)
-
-print(heuristic)
+assert type(data['A'][0]) == int, "dataset value expected datatype: int, given: "+type(data['A'][0]).__name__
+assert type(heuristic['A']) == int, "heuristic value expected datatype: int, given: "+type(heuristic['A']).__name__
